8379a801ae74a6993f94bef002ddd0a6785189c1
Sam Forrester <SForrester@spartaglobal.com>
2019-11-05 21:55:28 +0000

    added comments to main.tf file so it's clear what I'm doing

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
+# main code for terraform automation of deployment of running app
+# all variables interpolated for abstraction reasons and security reasons so this could be posted to github with a .gitignore
+
+# set the provider
+
 provider "aws" {
   region = "eu-west-1"
 }
 
-# launch first ec2 instance
+# launch the subnet you want your instance to be in
 
 resource "aws_subnet" "app_subnet" {
   vpc_id     = "${var.vpc_id}"
@@ -11,6 +16,9 @@ resource "aws_subnet" "app_subnet" {
     Name = "${var.name}_app_subnet"
   }
 }
+
+# launch first ec2 instance
+# assign it to the created subnet
 
 resource "aws_instance" "app_instance" {
   ami = "${var.app_ami_id}"
@@ -25,10 +33,13 @@ resource "aws_instance" "app_instance" {
   }
 }
 
+# launch security group for your instance and assign it in the instance code block
+
 resource "aws_security_group" "app_sg" {
     name = "allow port 80"
     description = "Allow traffic to pass from the private subnet to the internet"
 
+    #define the ports you want to open and from which IPs (with CIDR blocks)
     ingress {
         from_port = 80
         to_port = 80
@@ -67,6 +78,8 @@ resource "aws_security_group" "app_sg" {
         description = "open 3000 port from sam home"
     }
 
+    # set the vpc you want to use
+
     vpc_id = "${var.vpc_id}"
 
     tags = {
@@ -74,6 +87,7 @@ resource "aws_security_group" "app_sg" {
     }
 }
 
+# set the route table
 resource "aws_route_table" "app_route_table" {
     vpc_id = "${var.vpc_id}"
 
@@ -87,6 +101,8 @@ resource "aws_route_table" "app_route_table" {
     }
 }
 
+# associate the route table with a subnet
+
 resource "aws_route_table_association" "app_route_assoc" {
     subnet_id = "${aws_subnet.app_subnet.id}"
     route_table_id = "${aws_route_table.app_route_table.id}"
@@ -95,6 +111,8 @@ resource "aws_route_table_association" "app_route_assoc" {
 
 # Data command (get data from the aws service, store it to a variable)
 # Grab a reference to the internet gateway_id for our vpc
+# assign it to the route table back up in that block of code
+
 data "aws_internet_gateway" "default" {
   filter {
     name = "attachment.vpc-id"
@@ -102,10 +120,13 @@ data "aws_internet_gateway" "default" {
   }
 }
 
-# sending script
+# sending script for bash commands (See init.sh.tpl file in scripts/)
 data "template_file" "app_instance" {
   template = "${file("./scripts/app/init.sh.tpl")}"
 }
+
+# set an ssh key, with the name, and provide it with the public key for that pair.
+# Make sure you have the private part saved as a .pem for it in case you need to access it
 
 resource "aws_key_pair" "deployer" {
   key_name   = "${var.ssh_key_name}"
